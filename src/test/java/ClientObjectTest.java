503228ae6ec571b81e61b225fbafce5791b89a8a
Gaute Lindkvist <lindkvis@gmail.com>
2021-12-14 14:15:02 +0100

    Update gradle to new versions

--- src/test/java/ClientObjectTest.java
+++ src/test/java/ClientObjectTest.java
@@ -28,7 +28,7 @@ public class ClientObjectTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        testApp = new GrpcClientApp("localhost", 55555);
+        testApp = new GrpcClientApp("localhost", 50000);
     }
 
     @AfterEach
