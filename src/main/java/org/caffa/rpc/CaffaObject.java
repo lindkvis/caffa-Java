b744f99384e204c7223b037bce1edeb19820e9f0
Gaute Lindkvist <lindkvis@gmail.com>
2022-03-03 12:51:23 +0100

    Support simplified JSON

--- src/main/java/org/caffa/rpc/CaffaObject.java
+++ src/main/java/org/caffa/rpc/CaffaObject.java
@@ -51,8 +51,10 @@ public class CaffaObject {
 
     public ArrayList<CaffaObject> children() {
         ArrayList<CaffaObject> allChildren = new ArrayList<CaffaObject>();
+        System.out.println("Fields size: " + fields.size());
         for (Map.Entry<String, CaffaField<?>> entry : fields.entrySet()) {
             CaffaField<?> field = entry.getValue();
+            System.out.println(field.keyword);
             logger.log(Level.FINER, "Getting children from field: " + entry.getKey());
             allChildren.addAll(field.children());
             logger.log(Level.FINER, "Got children from field: " + entry.getKey());
@@ -114,8 +116,8 @@ public class CaffaObject {
 
     public String getAddressJson() {
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("classKeyword", this.classKeyword);
-        jsonObject.addProperty("uuid", this.uuid);
+        jsonObject.addProperty("Class", this.classKeyword);
+        jsonObject.addProperty("UUID", this.uuid);
         return jsonObject.toString();
     }
 
